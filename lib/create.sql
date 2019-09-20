b095960abe783e2f7724c54df2d2628e9d1ac701
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-20 17:34:18 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -6,4 +6,8 @@ CREATE TABLE bears (
   ,color TEXT
   ,temperament TEXT
   ,alive INTEGER
+<<<<<<< HEAD
 );
+=======
+);
+>>>>>>> d2e9b14fff54c45e79ddf7e2c06ccf67b26029d6
--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', "dark brown", "calm", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 20, 'M', "brown", "calm", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 9, 'F', "black", "sociable", 1);
@@ -7,3 +8,13 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 13, 'F', "grizzly", "wise", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 22, 'M', "black", "angry", 1);
 
+=======
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("mr. chocolate", 10, "male", "brown", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("rowdy", 20, "male", "grizzly", "calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("tabitha", 9, NULL, "black", "sociable", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "male", "black", "active", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("melissa", 17, NULL, "brown", "quiet", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("grinch", 11, "male", "brown", "peaceful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("wendy", 13 NULL, "grizzly", "wise", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 22 "male", "black", "angry", 1);
+>>>>>>> d2e9b14fff54c45e79ddf7e2c06ccf67b26029d6
